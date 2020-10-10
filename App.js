c7712c9b7364da6766447ad7f28cb3e7565e9dff
Tia Low <lowtia@gmail.com>
2020-10-10 15:01:55 -0700

    Trying to refresh

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
-      <Text style={styles.text}>How fast do you see this change?</Text>
+      <Text>SEE ME NOW?</Text>
+      <Image source={require('./assets/logo3.png')}/>
+      <Text style={styles.text}>SEE ME?</Text>
+      
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: 'white',
+    color: 'blue',
   },
 });
